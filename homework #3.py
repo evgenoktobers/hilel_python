0fd4fab3d2e6ea0045c2c7a3a98bd4e23ac8dc42
evgen <evgenoktobers@gmail.com>
2026-06-16 10:41:25 +0200

    test

--- homework #3.py
+++ homework #3.py
@@ -1,17 +1,29 @@
 #task 1 Найпростіший калькулятор
+#
+# first_number = int(input( "введіть перше число: "))
+# second_number = int(input( "введіть друге число: "))
+# math_action = input( "зазначте математичну дію: ")
+#
+# if math_action == "-":
+#     print(first_number - second_number)
+# elif math_action == "+":
+#     print(first_number + second_number)
+# elif math_action == "*":
+#     print(first_number * second_number)
+# elif math_action == "/":
+#     if second_number == 0:
+#         print("на нуль ділити поки що не можна")
+#     else: print(first_number / second_number)
+# else: print("неправильне значення")
 
-first_number = int(input( "введіть перше число: "))
-second_number = int(input( "введіть друге число: "))
-math_action = input( "зазначте математичну дію: ")
 
-if math_action == "-":
-    print(first_number - second_number)
-elif math_action == "+":
-    print(first_number + second_number)
-elif math_action == "*":
-    print(first_number * second_number)
-elif math_action == "/":
-    if second_number == 0:
-        print("на нуль ділити поки що не можна")
-    else: print(first_number / second_number)
-else: print("неправильне значення")
+#--------------------------------------------------------------
+#task 2 Перемістити елемент у списку
+# input_list = input("введіть числа через пробіл: ").split()
+#
+# if len(input_list) > 0:
+#     input_list.insert(0, input_list[-1])
+#     # print(nums)
+#     input_list.pop()
+#
+# print(input_list)
